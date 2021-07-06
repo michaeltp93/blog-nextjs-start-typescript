b9c16280aaad57940f1656f895364fbe77660b07
michaeltp93 <michael.trejosp@hotmail.es>
2021-07-06 14:52:10 +0200

    start template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/index.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+import type { ReactElement } from 'react';
+
+export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+	return <Component {...pageProps} />;
 }
-export default MyApp
